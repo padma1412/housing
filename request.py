c325a35cb51be016e2e0bb2bd3c5ea5c30fb08c3
Padmasree Padmanabhan <ext_ppadmanabhan@falabella.cl>
2021-09-07 00:36:21 +0530

    initial comit

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/predict_api'
+url = 'http://localhost:5000/predict'
 r = requests.post(url, json={'area': 3000, 'bedroom': 2, 'age': 15})
 
 print(r.json())
