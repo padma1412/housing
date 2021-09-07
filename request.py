81d4273b21285eccea14e5212e64489adcfb99d7
Padmasree Padmanabhan <ext_ppadmanabhan@falabella.cl>
2021-09-07 13:44:57 +0530

    changes

--- request.py
+++ request.py
@@ -1,6 +1,8 @@
 import requests
 
 url = 'http://localhost:5000/predict'
-r = requests.post(url, json={'area': 3000, 'bedroom': 2, 'age': 15})
+r = requests.post(url, json={'area': 3000, 'bedroom': 2, 'age': 15,'bathrooms':2,
+'basement':0,'stories':1,'parking':0,'areaperbedroom':1500,'guestroom':0,'bbratio':0.89})
+
 
 print(r.json())
